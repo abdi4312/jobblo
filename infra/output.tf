e15074e3d98d1b80bb6da9f350b323b42cd07808
Abdirahman Abdulle <abdirahman.abdulle@moller.no>
2025-04-24 20:40:32 +0200

    chore: adding credential for github action

--- infra/output.tf
+++ infra/output.tf
@@ -1,3 +1,23 @@
 output "acr_login_server" {
   value = azurerm_container_registry.jobblo_acr.login_server
 }
+output "acr_username" {
+  value = azurerm_container_registry.jobblo_acr.admin_username
+}
+
+output "acr_password" {
+  value     = azurerm_container_registry.jobblo_acr.admin_password
+  sensitive = true
+}
+
+output "client_id" {
+  value = azurerm_user_assigned_identity.github_identity.client_id
+}
+
+output "tenant_id" {
+  value = data.azurerm_client_config.current.tenant_id
+}
+
+output "subscription_id" {
+  value = data.azurerm_client_config.current.subscription_id
+}
