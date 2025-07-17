f129d82e0ac3a9d2eba721e017fc6726a01a6fcb
Abdirahman Abdulle <abdirahman.abdulle@moller.no>
2025-07-17 20:02:21 +0200

    chore: connect job api to the db test if it works

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -2,12 +2,14 @@ const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    description: { type: String },
-    category: { type: String },
+    description: String,
+    category: String,
     price: { type: Number, required: true },
-    location: { type: String },
-    imageUrl: { type: String },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, { timestamps: true });
+    location: String,
+    imageUrl: String,
+    user: { type: String, required: true }
+}, {
+    timestamps: true
+});
 
 module.exports = mongoose.model('Job', jobSchema);
