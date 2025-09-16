24bd7b2460a4fa160b823334744129810c0854fc
Abdirahman Abdulle <abdirahman.abdulle@moller.no>
2025-09-16 20:39:10 +0200

    new infra structure

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,14 +1,35 @@
 variable "resource_group_name" {
-  type    = string
-  default = "jobblo-rg"
+  type        = string
+  description = "Navn på resource group"
+  default     = "jobblo-rg"
 }
 
 variable "location" {
-  type    = string
-  default = "westeurope"
+  type        = string
+  description = "Azure region for ressursene"
+  default     = "westeurope"
 }
 
 variable "acr_name" {
-  type    = string
-  default = "jobbloacr123" # må være unikt globalt
+  type        = string
+  description = "Navn på Azure Container Registry"
+  default     = "jobbloacr" # unikt suffix legges til senere
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Navn på Azure Storage Account (må være globalt unikt)"
+  default     = "jobblostorage" # unikt suffix legges til senere
+}
+
+variable "github_repo" {
+  type        = string
+  description = "GitHub repo i format bruker/repo"
+  default     = "abdi4312/jobblo"
+}
+
+variable "github_main_branch" {
+  type        = string
+  description = "Main branch for GitHub Actions"
+  default     = "main"
 }
