031bbf9d6118f7c4a884e1dae062033ecf650485
abdi4312 <abdi4312@MD-P-5CG4390CBK.localdomain>
2025-11-24 11:15:09 +0100

    chore: add parameters to swagger

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
     customerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
