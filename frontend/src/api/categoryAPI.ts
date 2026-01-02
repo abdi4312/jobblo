90c34c15929d14cf5e547f3b1a86b19845a9b414
Brobo1 <armanahz95@gmail.com>
2026-01-02 19:10:42 +0100

    Remove outdated TODO comment in `categoryAPI.ts`

--- frontend/src/api/categoryAPI.ts
+++ frontend/src/api/categoryAPI.ts
@@ -1,6 +1,5 @@
 import mainLink from "./mainURLs";
 
-//TODO ask about function vs variable/const
 export async function getCategories() {
   const res = await mainLink.get("/api/categories");
   return res.data;
