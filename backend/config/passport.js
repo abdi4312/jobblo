e0dab477b444875ef925b9083b14391999e1ff74
Muhammad Ahmad <ahmadghouriweb@gmail.com>
2026-01-30 00:32:34 +0500

    chore: remove unused VippsStrategy import from passport config

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,5 +1,4 @@
 const passport = require('passport');
-const VippsStrategy = require('passport-vipps').Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
 
